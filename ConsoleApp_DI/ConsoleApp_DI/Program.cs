0aed612edbd9655b020d7940b5bb2ca9b1b4eb53
Suhas Parameshwara <suhasparameshwara@gmail.com>
2019-12-19 08:47:38 +0530

    DI_demo

---
+++ ConsoleApp_DI/ConsoleApp_DI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using AutoMapper;
+using log4net;
+
+namespace ConsoleApp_DI
+{
+	class Program
+	{
+		static IContainer Container { get; set; }
+		static void Main(string[] args)
+		{
+			Container = BuildContainer();
+			var log = Container.Resolve<LogProvider>();
+			log.GetCurrentClassLogger();
+		}
+
+
+		static IContainer BuildContainer()
+		{
+			var builder = new ContainerBuilder();
+			builder.RegisterType<LogProvider>().As<ILogger>();
+			return builder.Build();
+		}
+	}
+}
